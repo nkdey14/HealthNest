43e503c1016861fa39b825663bbc3e5671a0826d
nkdey14 <im.pandey264@gmail.com>
2026-04-23 12:49:14 +0530

    Unit Testing using Junit and Mockito for UserService class

--- src/main/java/com/doctor_service/controller/SearchController.java
+++ src/main/java/com/doctor_service/controller/SearchController.java
@@ -5,11 +5,11 @@ import com.doctor_service.entity.DoctorAppointmentSchedule;
 import com.doctor_service.entity.TimeSlots;
 import com.doctor_service.repository.DoctorRepository;
 import com.doctor_service.repository.TimeSlotsRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -31,22 +31,44 @@ public class SearchController {
             @RequestParam String specialization,
             @RequestParam String areaName
     ) {
+        LocalDate today = LocalDate.now();
+
         List<Doctor> doctors =
                 doctorRepository.findBySpecializationAndArea(specialization, areaName);
 
         for (Doctor doctor : doctors) {
+            List<LocalTime> allTimeSlots = new ArrayList<>();
+
             List<DoctorAppointmentSchedule> appointmentSchedules =
                     doctor.getAppointmentSchedules();
 
             for (DoctorAppointmentSchedule schedule : appointmentSchedules) {
+                LocalDate scheduleDate = schedule.getDate();
+                LocalTime now = LocalTime.now();
+
                 List<TimeSlots> timeSlots =
                         timeSlotsRepository.getAllTimeSlots(schedule.getId());
 
-                // IMPORTANT: attach timeslots to schedule if required
-                schedule.setTimeSlots(timeSlots);
+                for(TimeSlots timeSlot : timeSlots) {
+                    LocalTime slotTime = timeSlot.getTime();
+
+                    if (scheduleDate.isEqual(today)){
+                        if(slotTime.isAfter(now)){
+                            allTimeSlots.add(slotTime);
+                        }
+                    } else if (scheduleDate.isAfter(today)) {
+                            allTimeSlots.add(slotTime);
+                    }
+                }
             }
         }
 
         return doctors;
+    }
+
+    // http://localhost:8081/api/v1/doctor/getDoctorById?id=1
+    @GetMapping("/getDoctorById")
+    public Doctor getDoctorById(@RequestParam long id){
+        return doctorRepository.findById(id).get();
     }
 }
